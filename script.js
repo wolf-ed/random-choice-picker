4cc42294c9a979b0b10934aa44150b0044869ca1
wolf-ed <wolf7ed@gmail.com>
2022-02-16 23:23:06 +0100

    finished

--- script.js
+++ script.js
@@ -2,8 +2,49 @@ const tagsEl = document.getElementById('tags');
 const textArea = document.getElementById('textarea');
 textArea.focus()
 
+const pickRandomTag = () => {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+const highlightTag = (tag) => {
+    tag.classList.add('highlight')
+}
+
+const unHighlightTag = (tag) => {
+    tag.classList.remove('highlight')
+}
+
+const randomSelect = () => {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100)
+
+    setTimeout(() => {
+        clearInterval(interval)
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
+}
+
 textArea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+        randomSelect()
+    }
 })
 
 const createTags = (input) => {

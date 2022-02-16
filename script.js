a673bb9d7f31132782e0fe6695b523d45c50aca6
wolf-ed <wolf7ed@gmail.com>
2022-02-16 22:43:31 +0100

    script tags

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ textArea.addEventListener('keyup', (e) => {
 })
 
 const createTags = (input) => {
-    console.log(input)
+    const tags = input.split(',')
+        .filter(tag => tag.trim() !== '')
+        .map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl);
+    })
 }
